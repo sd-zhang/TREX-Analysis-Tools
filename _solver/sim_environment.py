fd2f69fdf167f4ae94d50445bcbb395a0731e060
Steven <shida1@ualberta.ca>
2021-08-11 13:14:26 -0600

    smarter action space initialization

--- _solver/sim_environment.py
+++ _solver/sim_environment.py
@@ -40,6 +40,7 @@ class SimulationEnvironment:
         table = db[table_name]
         p = table.find(table.table.columns.tstamp.between(study['start_timestamp'], study['end_timestamp']))
         self.participants[participant]['profile'] = list(p)
+        # print(self.participants[participant]['profile'][0])
 
     def __setup_actions(self, participant):
         trader = self.participants[participant]['trader']
@@ -47,13 +48,29 @@ class SimulationEnvironment:
             trader['actions'] = {}
         actions = trader['actions']
 
+        # crawl through the profile and figure out the maximum quantity to avoid too many "equivalent" states
+        max_generation = 0
+        max_consumption = 0
+        for row in self.participants[participant]['profile']:
+            generation, consumption = process_profile(row,
+                            gen_scale=self.participants[participant]['generation']['scale'],
+                            load_scale=self.participants[participant]['load']['scale'])
+            max_generation = max(max_generation, generation)
+            max_consumption = max(max_consumption, consumption)
+        max_qty = max(max_generation, max_consumption)
+
         # hard code actions for now. Future versions will utilize config file.
         if 'price' not in actions or not actions['price']:
             # actions['price'] = list(np.linspace(trader['bid_price'], trader['ask_price'], 15))
-            actions['price'] = list(np.linspace(0.05, 0.16, 17))
+            # actions['price'] = list(np.round(np.linspace(0.05, 0.16, 15), 5))
+
+            # limit prices to be between grid
+            grid_price_sell = self.configs['market']['grid']['price']
+            grid_price_buy = grid_price_sell * (1 + self.configs['market']['grid']['fee_ratio'])
+            actions['price'] = list(np.round(np.linspace(grid_price_sell, grid_price_buy, 9), 5))
 
         if 'quantity' not in actions or not actions['quantity']:
-            actions['quantity'] = list(range(7, 40, 1))
+            actions['quantity'] = list(range(0, max_qty+1, 1))
 
         # actions['price'] = tuple(np.linspace(trader['bid_price'], trader['ask_price']imp, 3))
         # actions['price'] = tuple(np.array([0.1]))
@@ -63,7 +80,12 @@ class SimulationEnvironment:
 
         if 'storage' in self.participants[participant]:
             if 'battery' not in actions or not actions['battery']:
-                actions['battery'] = tuple(range(-20, 20, 1))
+                # actions['battery'] = tuple(range(-20, 20, 1))
+                actions['battery'] = list(range(-max_qty, max_qty+1, 1))
+
+                # temporarily disable price and quantity to speed up self consumption sim
+                # actions['price'] = [0]
+                # actions['quantity'] = [0]
 
     def __setup_storage(self, participant):
         # convert storage params to Storage object
@@ -106,6 +128,8 @@ class SimulationEnvironment:
             net_load = consumption - generation
             t_start = row['tstamp'] - 60
             t_end = row['tstamp']
+
+            # print(participant, t_end, generation, consumption)
 
             if net_load > 0:
                 action_type = 'bids'
